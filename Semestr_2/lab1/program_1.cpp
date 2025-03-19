8da4660cc30b42bab06c90c8bca50a8ea1fdc468
Krzysztof Wojczakowski <unbreakable.programmist@gmail.com>
2025-03-19 17:26:21 +0100

    pierwsze laby 1

--- Semestr_2/lab1/program_1.cpp
+++ Semestr_2/lab1/program_1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include "vars.hpp"
 using namespace std;
 
 int mian(){
---
+++ Semestr_2/lab1/vars.hpp
@@ -0,0 +1,8 @@
+#ifdef VARS_HPP
+#define VARS_HPP
+
+extern int global_extern;
+void inc_vars();
+void print_vars();
+
+#endif
