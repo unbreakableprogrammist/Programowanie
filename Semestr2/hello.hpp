10b6edcbf2c13d9f03f84e1aa48ed8da3fce513d
Krzysztof Wojczakowski <unbreakable.programmist@gmail.com>
2025-03-19 17:10:53 +0100

    upgarde

---
+++ Semestr2/hello.hpp
@@ -0,0 +1,12 @@
+
+#ifndef HELLO_H
+#define HELLO_H
+#include<iostream>
+#include<string>
+namespace HELLO
+{
+    void print(std::string name);
+}
+
+
+#endif //HELLO_H
---
+++ Semestr2/strings.h
@@ -0,0 +1,15 @@
+//
+// Created by wojczakowskik on 3/7/25.
+//
+
+#ifndef STRINGS_H
+#define STRINGS_H
+#include<string>
+using namespace std;
+
+
+namespace strings{
+    string combine (string a, string b);
+}
+
+#endif //STRINGS_H
