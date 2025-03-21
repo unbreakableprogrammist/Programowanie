4fd03fac581d2f2436b915c40cd3a8a592ed9ccb
Krzysztof Wojczakowski <unbreakable.programmist@gmail.com>
2025-03-21 10:02:18 +0100

    dodalem memcpy i memmove

--- Semestr_2/wyk2/test.cpp
+++ Semestr_2/wyk2/test.cpp
@@ -1,13 +1,13 @@
 /*
-1. operatorach `new`, `new[]`, `delete`, `delete[]`,
+1. operatorach `new`, `new[]`, `delete`, `delete[]`, ok
 
-2. referencjach,
+2. referencjach, ok
 
-4. typach `enum class`,  
+4. typach `enum class`,   ok
 
-5. typie `std::byte`,
+5. typie `std::byte`, ok 
 
-6. funkcjach `memcpy`, `memmove`,
+6. funkcjach `memcpy`, `memmove`,  
 
 7. `static_cast`.
 
@@ -15,6 +15,7 @@
 #include<iostream>
 #include<iomanip>
 #include <cstddef> // bytes
+#include <cstring> // memcpy, memmove
 using namespace std;
 
 enum class owoce{jablko , pomarancza , sliwka };
@@ -33,15 +34,26 @@ int main(){
     delete[] tab;
     int p= 1030;
     int& pref = p;
-    cout<<pref;
+    //cout<<pref;
 
     int x = 1234;
     std::byte* bytes= reinterpret_cast<std::byte*>(&x);
     for (size_t i = 0; i < sizeof(x); ++i) {
-        std::cout << "Byte " << i << ": "
-                  << "0x" << std::hex << setw(2) << std::setfill('0')
-                  << static_cast<int>(bytes[i]) << "\n";
+        cout << "Byte in decimal " << i << ": "<<static_cast<int>(bytes[i]) << "\n";
+        //std::cout << "Byte " << i << ": "<< "0x" << std::hex << setw(2) << std::setfill('0')<< static_cast<int>(bytes[i]) << "\n";   
     }
+    char src[] = "Hello, world!";
+    char dest[50];
+
+    memcpy(dest, src, strlen(src) + 1);  // Kopiuje tekst, w tym znak '\0'
+
+    std::cout << "Copied string: " << dest << std::endl;
+    char str2[] = "Hello, world!";
+
+    // Zmieniamy tekst w miejscu
+    memmove(str2 + 6, str2, 5);  // Kopiujemy pierwsze 5 znaków na pozycję 6
+
+    std::cout << "Moved string: " << str2 << std::endl;
 
 }
 
