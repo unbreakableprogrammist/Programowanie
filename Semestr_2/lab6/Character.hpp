f89ef3ed8380ac241a59d3e9ce9910d817046673
Krzysztof Wojczakowski <criskbp.2019@gmail.com>
2025-06-06 10:52:22 +0200

    denerwuja mnie powiadomienia

--- Semestr_2/lab6/Character.hpp
+++ Semestr_2/lab6/Character.hpp
@@ -1,1 +1,28 @@
 #include<iostream>
+using namespace std;
+class Character
+{
+    private:
+        string name;
+        int health;
+        int maxHealth;
+    public : 
+        Character(string name, int health = 100);
+        string getName() const { return name; }
+        int getHealth() const { return health; }
+        bool isAlive() const { return health > 0; }
+        void takeDamage(int damage);
+        void heal(int amount);
+        virtual void attack(Character* target);
+        virtual ~Character() {}
+
+    };
+    class Warrior : public Warrior
+    {
+        private:
+            int meleeDamage;
+        public:
+            Warrior(string name, int health = 120, int damage = 15);
+            void attack(Character* target);
+            ~Warrior() {}
+    };
